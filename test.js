0d0d9d998a3c56edd308974f4b0ee638bc1b866d
FGabriel <Gabriel@forapulse.com>
2024-02-26 23:29:58 +0100

    test2

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-const { DBSpecies } = require('./database/createDatabase');
-
 function isSpeciesFilled() {
-	const toTest = DBSpecies.findAll();
-
-	if (toTest.length == 0) {
-		console.log('La DB n\'a pas été setup correctement');
-	}
-	else {
-		console.log('La DB a été setup correctement');
-	}
+	console.log('ceci est un test');
 
 	return;
 }
